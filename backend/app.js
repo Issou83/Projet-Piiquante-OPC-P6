e69609b9e2826a6a35fe5b97f3d9746f109467ae
Issou83 <issou8@gmail.com>
2023-02-04 15:39:52 +0100

    Ajout des CORS dans app.js et installation de mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,24 @@
 const express = require('express');
 
 const app = express();
+const mongoose = require('mongoose');
+
+/*Pour permettre des requêtes cross-origin (et empêcher des erreurs CORS), des headers spécifiques de 
+contrôle d'accès doivent être précisés pour tous vos objets de réponse.*/
+
+/*Le CORS définit comment les serveurs et les navigateurs interagissent, en spécifiant quelles 
+ressources peuvent être demandées de manière légitime – par défaut, les requêtes AJAX sont interdites.
+
+Configurer les bons headers sur l'objet réponse permet l'envoi et la réception de requêtes et de réponses sans erreurs CORS.*/
+
+// On autorise les requêtes CORS (Cross-Origin Resource Sharing) depuis n'importe quelle origine
+//Le middleware ne prend pas d'adresse en premier paramètre, afin de s'appliquer à toutes les routes
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    next();
+  });
 
 app.use((req, res, next) => {
   console.log('Requête reçue !');
